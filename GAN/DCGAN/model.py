c422be1359dce0e66e2a0c1e98a18606b2e27e86
sinmugyeol <tlsanruf123@gmail.com>
2021-07-24 16:07:56 +0900

    add weights_init

--- GAN/DCGAN/model.py
+++ GAN/DCGAN/model.py
@@ -41,10 +41,12 @@ ngpu = 0 ## 사용 가능한 GPU의 수 CPU 모드에는 0을 사용
 
 ## 데이터셋 생성
 dataset = dset.ImageFolder(root=dataroot, transform=transforms.Compose([
-    transforms.Resize(image_size), ##
-    transforms.CenterCrop(image_size),
-    transforms.ToTensor(),
+    transforms.Resize(image_size), ## 이미지 사이즈 재설정
+    transforms.CenterCrop(image_size), ## 중심을 위주로 자르기
+    transforms.ToTensor(), ## 탠서로 변환
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ## 평균과 표준편차를 사용하여 텐서 이미지를 정규화
+    ## (평균, 표준) 각 채널에 대한 평균 시퀸스, 각 채널에 대한 표준 편차의 시퀸스
 ]))
 
 ## 데이터 로더 생성
@@ -60,4 +62,13 @@ plt.axis("off")
 plt.title("Training Images")
 plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
 plt.show()
+
+## Generator 와 Discriminator에서 호출될 가중치 초기화
+def weights_init(m):
+    classname = m.__class__.__name__ ## m의 클래스 이름 참조
+    if classname.find('Conv') != -1: ## 클래스 이름에 Conv가 있을경우 True
+        nn.init.normal_(m.weight.data, 0.0, 0.02) ## 입력 텐서를 정규 분포에서 가져온 값으로 채운다.
+    elif classname.find('BatchNorm') != -1: ## 클래스 이름에 BatchNorm이 있을경우 True
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0) ## 주어진 텐서서 자리에 스칼라 값을 넣는다
 
