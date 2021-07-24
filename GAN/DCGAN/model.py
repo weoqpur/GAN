8b358db6d7372b8b6246da0e6c928a175a874cbd
sinmugyeol <tlsanruf123@gmail.com>
2021-07-24 14:31:54 +0900

    dataset and dataloader add

--- GAN/DCGAN/model.py
+++ GAN/DCGAN/model.py
@@ -25,7 +25,7 @@ random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
 
-dataroot = "./img_align_celeba" ## 데이터 셋의 경로
+dataroot = "./celeba" ## 데이터 셋의 경로
 workers = 2 ## 데이터로더의 worker 수
 batch_size = 128 ## 배치 사이즈 설정
 image_size = 64 ## 이미지의 크기 설정
@@ -37,3 +37,27 @@ num_epochs = 5 ## training epochs의 수
 lr = 0.0002 ## Learning rate
 beta1 = 0.5 ## Adam optimizers를 위한 hyperparameter
 ngpu = 0 ## 사용 가능한 GPU의 수 CPU 모드에는 0을 사용
+
+
+## 데이터셋 생성
+dataset = dset.ImageFolder(root=dataroot, transform=transforms.Compose([
+    transforms.Resize(image_size), ##
+    transforms.CenterCrop(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+]))
+
+## 데이터 로더 생성
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+## GPU 사용 여부 결정
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# 일부 훈련 이미지 보기
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.show()
+
