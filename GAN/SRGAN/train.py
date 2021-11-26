c11081ee94225d1ef21b7a64c70b7e8d4c06b0e3
sinmugyeol <tlsanruf123@gmail.com>
2021-11-26 23:30:54 +0900

    [UPDATE] validation part

--- GAN/SRGAN/train.py
+++ GAN/SRGAN/train.py
@@ -2,13 +2,16 @@ import argparse
 import os
 from math import log10
 
+import numpy as np
 import pandas as pd
 import torch.optim as optim
 import torch.utils.data
 import torchvision.utils as utils
+import torchvision.transforms as transforms
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from PIL import Image
 
 import ssim
 from data_loader import TrainDataset, ValDataset, display_transform
@@ -57,57 +60,118 @@ for epoch in range(1, num_epochs + 1):
     # network를 훈련모드로 전환
     netG.train()
     netD.train()
-    with torch.autograd.set_detect_anomaly(True):
-        for data, target in train_bar:
-            ### g_update_first = True
-            batch_size = data.size(0)
-            # batch size 계산
-            running_results['batch_sizes'] += batch_size
-            # (1) Update D network: maximize D(x)-1-D(G(z))
-            real_img = Variable(target)
+    for data, target in train_bar:
+        ### g_update_first = True
+        batch_size = data.size(0)
+        # batch size 계산
+        running_results['batch_sizes'] += batch_size
+        # (1) Update D network: maximize D(x)-1-D(G(z))
+        real_img = Variable(target)
+        if torch.cuda.is_available():
+            real_img = real_img.cuda()
+        z = Variable(data)
+        if torch.cuda.is_available():
+            z = z.cuda()
+        fake_img = netG(z)
+
+        netD.zero_grad()
+        real_out = netD(real_img).mean()
+        fake_out = netD(fake_img).mean()
+        d_loss = 1 - real_out + fake_out
+        d_loss.backward(retain_graph=True)
+        optimizerD.step()
+
+        # (2) Update G network: minimize 1-D(G(z)) + Perception Loss + Image Loss + TV Loss
+        netG.zero_grad()
+        ## 런타임 에러 방지 코드
+        fake_img = netG(z)
+        fake_out = netD(fake_img).mean()
+        ##
+        g_loss = generator_criterion(fake_out, fake_img, real_img)
+        g_loss.backward()
+
+        fake_img = netG(z)
+        fake_out = netD(fake_img).mean()
+
+        optimizerG.step()
+
+        # 최적화 전 loss
+        running_results['g_loss'] += g_loss.item() * batch_size
+        running_results['d_loss'] += d_loss.item() * batch_size
+        running_results['d_score'] += real_out.item() * batch_size
+        running_results['g_score'] += fake_out.item() * batch_size
+
+        train_bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f' % (
+            epoch, num_epochs, running_results['d_loss'] / running_results['batch_sizes'],
+            running_results['g_loss'] / running_results['batch_sizes'],
+            running_results['d_score'] / running_results['batch_sizes'],
+            running_results['g_score'] / running_results['batch_sizes']
+        ))
+    netG.eval()
+    out_path = 'training_results/SRF_' + str(upscale_factor) + '/'
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    # validation
+    with torch.no_grad():
+        val_bar = tqdm(val_loader)
+        valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
+        val_images = list()
+        toTensor = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor()
+        ])
+        for val_lr, val_hr_restore, val_hr in val_bar:
+            batch_size = val_lr.size(0)
+            valing_results['batch_sizes'] += batch_size
+            lr = val_lr
+            hr = val_hr
             if torch.cuda.is_available():
-                real_img = real_img.cuda()
-            z = Variable(data)
-            if torch.cuda.is_available():
-                z = z.cuda()
-            fake_img = netG(z)
+                lr = lr.cuda()
+                hr = hr.cuda()
+            sr = netG(lr)
 
-            netD.zero_grad()
-            real_out = netD(real_img).mean()
-            fake_out = netD(fake_img).mean()
-            d_loss = 1 - real_out + fake_out
-            d_loss.backward(retain_graph=True)
-            optimizerD.step()
+            batch_mse = ((sr - hr) ** 2).data.mean()
+            valing_results['mse'] += batch_mse * batch_size
+            batch_ssim = ssim.ssim(sr, hr).item()
+            valing_results['ssims'] += batch_ssim * batch_size
+            valing_results['psnr'] = 10 * log10((hr.max()**2) / (valing_results['mse'] / valing_results['batch_sizes']))
+            valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
+            val_bar.set_description(
+                desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
+                    valing_results['psnr'], valing_results['ssim']
+                ))
+            val_images.extend(
+                [display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
+                 display_transform()(sr.data.cpu().squeeze(0))]
+            )
+        val_images = torch.stack(val_images)
+        val_images = torch.chunk(val_images, 1)
+        val_save_bar = tqdm(val_images, desc='[saving training results]')
+        index = 1
+        for image in val_save_bar:
+            image = utils.make_grid(image, nrow=3, padding=5)
+            utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
+            index += 1
 
-            # (2) Update G network: minimize 1-D(G(z)) + Perception Loss + Image Loss + TV Loss
-            netG.zero_grad()
-            print("fake image 1" + str(fake_img.shape))
-            ## 코랩 런타임 에러 방지 코드
-            # fake_img = netG(z)
-            # fake_out = netD(fake_img).mean()
-            ##
-            g_loss = generator_criterion(fake_out.clone(), fake_img.clone(), real_img.clone())
-            print(g_loss)
-            g_loss.backward()
+    # save model parameters
+    torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_%d.pth' % (upscale_factor, epoch))
+    torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_%d.pth' % (upscale_factor, epoch))
+    # save loss,scores,psnr,ssim
+    results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
+    results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
+    results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
+    results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
+    results['psnr'].append(valing_results['psnr'])
+    results['ssim'].append(valing_results['ssim'])
 
-            fake_img = netG(z)
-            print("fake image 2" + str(fake_img.shape))
-            fake_out = netD(fake_img).mean()
-
-            optimizerG.step()
-
-            # 최적화 전 loss
-            running_results['g_loss'] += g_loss.item() * batch_size
-            running_results['d_loss'] += d_loss.item() * batch_size
-            running_results['d_score'] += real_out.item() * batch_size
-            running_results['g_score'] += fake_out.item() * batch_size
-
-            train_bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f' % (
-                epoch, num_epochs, running_results['d_loss'] / running_results['batch_sizes'],
-                running_results['g_loss'] / running_results['batch_sizes'],
-                running_results['d_score'] / running_results['batch_sizes'],
-                running_results['g_score'] / running_results['batch_sizes']
-            ))
-
+    if epoch % 10 == 0 and epoch != 0:
+        out_path = 'statistics/'
+        data_frame = pd.DataFrame(
+            data={'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'],
+                  'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim']},
+            index=range(1, epoch + 1)
+        )
+        data_frame.to_csv(out_path + 'srf_' + str(upscale_factor) + '_train_results.csv', index_label='Epoch')
 
 print("성공")
