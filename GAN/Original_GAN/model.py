746fb93d4236096a643a558b037fc03192f3176b
sinmugyeol <tlsanruf123@gmail.com>
2021-12-22 21:03:15 +0900

    [UPDATE] number of channel

--- GAN/Original_GAN/model.py
+++ GAN/Original_GAN/model.py
@@ -1,91 +1,60 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
 # 생성자 만들기 (Generator)
 class Generator(nn.Module):
     # 초기값 설정 - 인풋 채널, 아웃풋 채널, normalization
-    def __init__(self, in_channels, out_channels, norm='bnorm'):
+    def __init__(self, img_shape):
         super(Generator, self).__init__()
 
-        # decoder
-        self.dec1 = DECBR2d(1 * in_channels, 8 * 64, kernel_size=4, stride=1, padding=0, norm=norm, relu=0.0, bias=False)
-        self.dec2 = DECBR2d(8 * 64, 4 * 64, kernel_size=4, stride=2, padding=1, norm=norm, relu=0.0, bias=False)
-        self.dec3 = DECBR2d(4 * 64, 2 * 64, kernel_size=4, stride=2, padding=1, norm=norm, relu=0.0, bias=False)
-        self.dec4 = DECBR2d(2 * 64, 64, kernel_size=4, stride=2, padding=1, norm=norm, relu=0.0, bias=False)
-        self.dec5 = DECBR2d(64, out_channels, kernel_size=4, stride=2, padding=1, norm=None, relu=None, bias=False)
+        self.block1 = Block(100, 64 * 2, norm=False)
+        self.block2 = Block(64 * 2, 64 * 4)
+        self.block3 = Block(64 * 4, 64 * 8)
+        self.block4 = Block(64 * 8, 64 * 16)
+        self.linear = nn.Linear(1024, 256),
         self.tanh = nn.Tanh()
 
-    # x는 인풋 이미지
     def forward(self, x):
-        x = self.dec1(x)
-        x = self.dec2(x)
-        x = self.dec3(x)
-        x = self.dec4(x)
-        x = self.dec5(x)
-
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.linear(x)
         x = self.tanh(x)
 
         return x
 
 # DCGAN 판별자(discriminator) 만들기
 class Discriminator(nn.Module):
-    # 초기값: 인풋 채널, 아웃풋 채널, normalization
-    def __init__(self, in_channels, out_channels, norm='bnorm'):
+    def __init__(self):
         super(Discriminator, self).__init__()
 
-        # Encoder
-        self.enc1 = CBR2d(1 * in_channels, 1 * 64, kernel_size=4, stride=2, padding=1, norm=norm, relu=0.2, bias=False)
-        self.enc2 = CBR2d(1 * 64, 2 * 64, kernel_size=4, stride=2, padding=1, norm=norm, relu=0.2, bias=False)
-        self.enc3 = CBR2d(2 * 64, 4 * 64, kernel_size=4, stride=2, padding=1, norm=norm, relu=0.2, bias=False)
-        self.enc4 = CBR2d(4 * 64, 8 * 64, kernel_size=4, stride=2, padding=1, norm=norm, relu=0.2, bias=False)
-        self.enc5 = CBR2d(8 * 64, out_channels, kernel_size=4, stride=2, padding=1, norm=None, relu=None, bias=False)
-        self.sig = nn.Sigmoid()
+        self.dis = nn.Sequential(
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
-        x = self.enc1(x)
-        x = self.enc2(x)
-        x = self.enc3(x)
-        x = self.enc4(x)
-        x = self.enc5(x)
-
-        x = self.sig(x)
-
+        x = x.view(x.size(0), -1)
+        x = self.dis(x)
         return x
 
-# Convolution - batch normalization - ReLU
-class CBR2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=4, stride=1, padding=0, bias=True, norm='bnorm', relu=0.0):
 
-        layers = []
-        layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
-                             stride=stride, padding=padding, bias=bias)]
-        if norm == 'bnorm':
-            layers += [nn.BatchNorm2d(num_features=out_channels)]
+class Block(nn.Module):
+    def __init__(self, in_channel, out_channel, norm=True):
+        super(Block, self).__init__()
+        layer = []
+        layer += [nn.Linear(in_features=in_channel, out_features=out_channel)]
+        if norm:
+            layer += [nn.BatchNorm2d(out_channel, 0.8)]
+        layer += (nn.LeakyReLU(0.2, inplace=True))
 
-        if not relu is None and relu >= 0.0:
-            layers += [nn.ReLU() if relu == 0 else nn.LeakyReLU(relu)]
+        self.block = nn.Sequential(*layer)
 
-        self.cbr = nn.Sequential(*layers)
-    # x: 인풋 이미지
     def forward(self, x):
-        return self.cbr(x)
-
-
-# Deconvolution - batch normalization - ReLU
-class DECBR2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=4, stride=1, padding=0, bias=True, norm='bnorm',
-                 relu=0.0):
-        layers = []
-        layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
-                             stride=stride, padding=padding, bias=bias)]
-        if norm == 'bnorm':
-            layers += [nn.BatchNorm2d(num_features=out_channels)]
-
-        if not relu is None and relu >= 0.0:
-            layers += [nn.ReLU() if relu == 0 else nn.LeakyReLU(relu)]
-
-        self.cbr = nn.Sequential(*layers)
-
-    # x: 인풋 이미지
-    def forward(self, x):
-        return self.cbr(x)
+        return self.block(x.cloen())
