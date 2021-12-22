ca4034535b14012096cb1ca663e8190964c8ff4c
sinmugyeol <tlsanruf123@gmail.com>
2021-12-22 21:03:45 +0900

    [UPDATE] train part

--- GAN/Original_GAN/train.py
+++ GAN/Original_GAN/train.py
@@ -5,10 +5,13 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torch.autograd import Variable
 
 from torchvision import transforms
-
+from dataset import Dataset
 from model import *
+from utils import init_weights
+import torch.optim as optim
 
 ## 기본적인 변수 설정
 lr = 2e-4
@@ -30,3 +33,79 @@ if not os.path.exists(result_dir_train):
 if not os.path.exists(result_dir_test):
     os.makedirs(os.path.join(result_dir_test, 'png'))
     os.makedirs(os.path.join(result_dir_test, 'numpy'))
+
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5])
+])
+
+train_set = Dataset(os.path.join(data_dir, 'pokemon', transform=transform))
+train_loader = torch.utils.data.DataLoader(train_set, batch_size, shuffle=True, num_workers=0)
+
+num_batch_train = int((train_set.__len__() / batch_size) + ((train_set.__len__() / batch_size) != 0))
+
+out_path = "training_results/"
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
+
+netG = Generator().to(device)
+netD = Discriminator().to(device)
+
+init_weights(netG)
+init_weights(netD)
+
+loss = nn.BCELoss().to(device)
+
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(0.5, 0.999))
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(0.5, 0.999))
+
+fn_tonumpy = lambda x: x.to('cpu').datach().numpy().transpose(0, 2, 3, 1)
+fn_denorm = lambda x, mean, std: (x * std) + mean
+
+
+
+for epoch in range(1, num_epoch + 1):
+    netG.train()
+    netD.train()
+
+    loss_train = []
+    loss_D_fake_train = []
+    loss_D_real_train = []
+
+    for batch, data in enumerate(train_loader, 1):
+
+        valid = Variable(torch.FloatTensor(data.size(0), 1).fill_(1.0), requires_grad=False)
+        fake = Variable(torch.FloatTensor(data.size(0), 1).fill_(0.0), requires_grad=False)
+
+        real_imgs = Variable(data.type(torch.FloatTensor))
+
+        optimizerG.zero_grad()
+
+        # noise as generator input
+        z = Variable(torch.FloatTensor(np.random.normal(0, 1, (data.shape[0], 100))))
+
+        gen_imgs = netG(z)
+
+        g_loss = loss(netD(gen_imgs), valid)
+
+        g_loss.backward()
+        optimizerG.step()
+
+        optimizerD.zero_grad()
+
+        real_loss = loss(netD(real_imgs), valid)
+        fake_loss = loss(netD(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizerD.step()
+
+        loss_train += [g_loss.item()]
+        loss_D_fake_train += [fake_loss.item()]
+        loss_D_real_train += [real_loss.item()]
+
+        print(
+            "train: epoch %04d / %04d | batch: %04d / %04d | GAN %.4f | DISC REAL: %.4f | DISC REAL: %.4f"
+            % (num_epoch, epoch, batch_size, batch, np.mean(loss_train), np.mean(loss_D_real_train),
+               np.mean(loss_D_fake_train)))
