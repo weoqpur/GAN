0457d58c0016d6bfd9ab115ab4f56ad9a743a658
sinmugyeol <tlsanruf123@gmail.com>
2022-01-12 22:02:37 +0900

    [UPDATE] body -> body1, body2, body3

--- GAN/Unconditional_GAN/SinGAN/SinGAN/model.py
+++ GAN/Unconditional_GAN/SinGAN/SinGAN/model.py
@@ -20,30 +20,26 @@ class CBR2d(nn.Module):
     def forward(self, x):
         return self.cbr(x)
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv2d') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('Norm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
-
 class WDiscriminator(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, padding, stride):
         super(WDiscriminator, self).__init__()
         channels = 32
         self.head = CBR2d(in_channels=in_channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
                           stride=stride)
-        self.body = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
-                          stride=stride)
+        self.body1 = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
+                           stride=stride)
+        self.body2 = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
+                           stride=stride)
+        self.body3 = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
+                           stride=stride)
         self.tail = nn.Conv2d(in_channels=channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding,
                               stride=stride)
 
     def forward(self, x):
         x = self.head(x)
-        x = self.body(x)
-        x = self.body(x)
-        x = self.body(x)
+        x = self.body1(x)
+        x = self.body2(x)
+        x = self.body3(x)
         x = self.tail(x)
         return x
 
@@ -53,16 +49,20 @@ class Generator(nn.Module):
         channels = 32
         self.head = CBR2d(in_channels=in_channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
                           stride=stride)
-        self.body = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
-                          stride=stride)
+        self.body1 = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
+                           stride=stride)
+        self.body2 = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
+                           stride=stride)
+        self.body3 = CBR2d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=padding,
+                           stride=stride)
         self.tail = nn.Conv2d(in_channels=channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
                               padding=padding)
 
     def forward(self, x, y):
         x = self.head(x)
-        x = self.body(x)
-        x = self.body(x)
-        x = self.body(x)
+        x = self.body1(x)
+        x = self.body2(x)
+        x = self.body3(x)
         x = self.tail(x)
         x = torch.tanh(x)
         ind = int((y.shape[2]-x.shape[2])/2)
